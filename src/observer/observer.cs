158a3ac719aef1e2c5899378a5b6925d1b36e323
Capella87 <contact@capelladev.com>
2022-08-23 23:04:03 +0900

    Add observer pattern

---
+++ src/observer/observer.cs
@@ -0,0 +1,96 @@
+public interface ISubject
+{
+    public void Register(ISubscriber subscriber);
+    public void Remove(ISubscriber subscriber);
+    public void Notify();
+}
+
+public sealed class WeatherData : ISubject
+{
+    float _temperature;
+    float _humidity;
+    float _pressure;
+
+    public float Temperature { get { return _temperature; } }
+    public float Humidity { get { return _humidity; } }
+    public float Pressure { get { return _pressure; } }
+
+    List<ISubscriber> _subscribers;
+
+    public WeatherData()
+    {
+        _subscribers = new List<ISubscriber>();
+    }
+
+    public void Register(ISubscriber subscriber)
+    {
+        _subscribers.Add(subscriber);
+    }
+
+    public void Remove(ISubscriber subscriber)
+    {
+        _subscribers.Remove(subscriber);
+    }
+
+    public void Notify()
+    {
+        foreach (var subscriber in _subscribers)
+            subscriber.Update();
+    }
+
+    public void SetData(float temperature, float humidity, float pressure)
+    {
+        _temperature = temperature;
+        _humidity = humidity;
+        _pressure = pressure;
+        Notify();
+    }
+}
+
+public interface ISubscriber
+{
+    public void Update();
+}
+
+public interface IDisplay
+{
+    public void Display();
+}
+
+public sealed class Subscriber : ISubscriber, IDisplay
+{
+    public WeatherData WeatherData { init; get; }
+    private float _temperature;
+    private float _humidity;
+
+    public Subscriber(WeatherData weatherData)
+    {
+        WeatherData = weatherData;
+        WeatherData.Register(this);
+    }
+
+    public void Update()
+    {
+        _temperature = WeatherData.Temperature;
+        _humidity = WeatherData.Humidity;
+        Display();
+    }
+
+    public void Display()
+    {
+        Console.WriteLine($"Current status: Temperature {_temperature}, Humidity: {_humidity}");
+    }
+}
+
+public static class Program
+{
+    public static void Main(string[] args)
+    {
+        var weatherData = new WeatherData();
+        var subscriber = new Subscriber(weatherData);
+
+        weatherData.SetData(120.0f, 40.4f, 30.4f);
+        weatherData.SetData(118.0f, 40.2f, 30.3f);
+        weatherData.SetData(121.0f, 40.1f, 30.2f);
+    }
+}
---
+++ src/observer/observer.java
@@ -0,0 +1,100 @@
+import java.util.ArrayList;
+import java.util.List;
+
+interface Subject {
+    public void registerObserver(Observer o);
+    public void removeObserver(Observer o);
+    public void notifyObservers();
+}
+
+class WeatherData implements Subject {
+    private List<Observer> observers;
+    private float temperature;
+    private float humidity;
+    private float pressure;
+
+    public WeatherData() {
+        observers = new ArrayList<Observer>();
+    }
+
+    public void registerObserver(Observer o) {
+        observers.add(o);
+    }
+
+    public void removeObserver(Observer o) {
+        observers.remove(o);
+    }
+
+    public void notifyObservers() {
+        for (Observer observer : observers) {
+            observer.update();
+        }
+    }
+
+    // 'Event'
+    public void measurementsChanged() {
+        notifyObservers();
+    }
+
+    public void setMeasurements(float temperature, float humidity, float pressure) {
+        this.temperature = temperature;
+        this.humidity = humidity;
+        this.pressure = pressure;
+        measurementsChanged();
+    }
+
+    public float getTemperature() {
+        return this.temperature;
+    }
+
+    public float getHumidity() {
+        return this.humidity;
+    }
+
+    public float getPressure() {
+        return this.pressure;
+    }
+}
+
+interface Observer {
+    public void update();
+}
+
+interface DisplayElement {
+    public void display();
+}
+
+class CurrentConditionsDisplay implements Observer, DisplayElement {
+    private float temperature;
+    private float humidity;
+
+    // Provider
+    private WeatherData weatherData;
+
+    public CurrentConditionsDisplay(WeatherData weatherData) {
+        this.weatherData = weatherData;
+        weatherData.registerObserver(this);
+    }
+
+    public void update() {
+        this.temperature = weatherData.getTemperature();
+        this.humidity = weatherData.getHumidity();
+        display();
+    }
+
+    public void display() {
+        System.out.println("Current status: Temperature " + temperature + "F, Humidity " + humidity + "%");
+    }
+}
+
+public class Main {
+    public static void main(String[] args) {
+        var weatherData = new WeatherData();
+
+        var currentDisplay = new CurrentConditionsDisplay(weatherData);
+
+        weatherData.setMeasurements(80, 65, 30.4f);
+        weatherData.setMeasurements(82, 70, 29.2f);
+        weatherData.setMeasurements(78, 90, 29.2f);
+    }
+}
